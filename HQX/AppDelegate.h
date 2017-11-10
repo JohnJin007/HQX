7bbfba75c245eb2d92d58f69bcd9d179ab815294
LeeJin <lx@lxdeMacBook-Pro.local>
2017-11-10 14:20:05 +0800

    Initial Commit

---
+++ HQX/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HQX
+//
+//  Created by lx on 2017/11/10.
+//  Copyright © 2017年 pingan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
